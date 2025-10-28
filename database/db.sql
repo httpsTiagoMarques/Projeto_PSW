5443c454a2a672e00f41c67ef22fd429b6776bf0
httpsTiagoMarques <studydummytm22@gmail.com>
2025-10-28 22:59:20 +0000

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -10,7 +10,6 @@ CREATE TABLE User (
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     createdOn DATE NOT NULL,
-    FOREIGN KEY (userRoleId) REFERENCES UserRole (id)
 );
 
 -- Criar tabela UserLog
