8be54abe64dada6b4767d9176da02b80600937c7
httpsTiagoMarques <studydummytm22@gmail.com>
2025-10-28 22:58:27 +0000

    Alteração bd

--- database/db.sql
+++ database/db.sql
@@ -3,16 +3,9 @@ CREATE DATABASE IF NOT EXISTS PSW_Projeto;
 
 USE PSW_Projeto;
 
--- Criar tabela UserRole
-CREATE TABLE UserRole (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    role VARCHAR(50) NOT NULL
-);
-
 -- Criar tabela User
 CREATE TABLE User (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    userRoleId INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
